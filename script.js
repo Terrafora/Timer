64c453f0c27097ff9ad76fe454f014dd19533084
Jack Stone <51866500+Terrafora@users.noreply.github.com>
2023-02-05 17:01:41 +0000

    basis working.

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const pomodoroTimer = document.getDocumentById("pomodoro-timer")
+const pomodoroTimer = document.getElementById("pomodoro-timer")
 
-const startButton = document.getDocumentById("pomodoro-start")
-const pauseButton = document.getDocumentById("pomodoro-pause")
-const stopButton = document.getDocumentById("pomodoro-stop")
+const startButton = document.getElementById("pomodoro-start")
+const pauseButton = document.getElementById("pomodoro-pause")
+const stopButton = document.getElementById("pomodoro-stop")
+
+let isClockRunning = false
+
+// 25 minutes in seconds
+
+let workSessionDuration = 1500
+let currentTimeLeftInSession = 1500
+
+// 5 minutes in seconds
+
+let breakSessionSuration = 300
 
 //Start button functionality
 
@@ -19,5 +30,54 @@ pauseButton.addEventListener("click", () => {
 //Stop button functionality
 
 stopButton.addEventListener("click", () => {
-    toggleClock()
+    toggleClock(true)
 })
+
+const toggleClock = (reset) => {
+    if(reset) {
+        // Stop the timer
+        stopClock()
+    }
+    else {
+        if(isClockRunning === true) {
+            // Pause the timer
+            clearInterval(clockTimer)
+            isClockRunning = false
+        }
+        else {
+            // Start the timer
+            isClockRunning = true
+                clockTimer = setInterval(()=> {
+                currentTimeLeftInSession--
+                displayCurrentTimeLeftInSession()
+            }, 1000)
+        }
+
+    }
+}
+
+const displayCurrentTimeLeftInSession = () => {
+    const secondsLeft = currentTimeLeftInSession
+    let result = ""
+    const seconds = secondsLeft % 60
+    const minutes = parseInt(secondsLeft / 60) % 60
+    let hours = parseInt(secondsLeft / 3600)
+
+    // Add 0s if at less than 10
+
+    function addLeadingZeroes(time) {
+        return time < 10 ? `0${time}` : time
+    }
+
+    if (hours > 0) result += `${hours}:`
+    result += `${addLeadingZeroes(minutes)}:${addLeadingZeroes(seconds)}`
+    pomodoroTimer.innerText = result.toString()
+}
+
+const stopClock = () => {
+    // Reset timer
+    clearInterval(clockTimer)
+    isClockRunning = false
+    currentTimeLeftInSession = workSessionDuration
+    displayCurrentTimeLeftInSession()
+}
